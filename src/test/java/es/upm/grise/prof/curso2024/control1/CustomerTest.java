b456a8cc17602a1d9c46a099408a49a63e1bc277
DESKTOP-UTS5A7O\Francisco del Puerto <fjdelpuertocarrizosa@gmail.com>
2024-12-12 20:05:28 +0100

    reconfigurado gradle

--- src/test/java/es/upm/grise/prof/curso2024/control1/CustomerTest.java
+++ src/test/java/es/upm/grise/prof/curso2024/control1/CustomerTest.java
@@ -1,51 +1,49 @@
 package es.upm.grise.prof.curso2024.control1;
 
 import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
 
 public class CustomerTest {
+	
+	@Test
+	public void testThrowsExceptionWhenNoAccounts() {
 
-    @Test
-    public void testThrowsExceptionWhenNoAccounts() {
+		Customer customer = new Customer();
+		assertThrows(NoAccountsException.class, () -> {
+			customer.getAccountWithHighestBalance();
+		});
+	}
 
-        Customer customer = new Customer();
-        assertThrows(NoAccountsException.class, () -> {
-            customer.getAccountWithHighestBalance();
-        });
-    }
-    
-    
-    @Test
-    public void testGetAccountNumber() throws NoAccountsException, NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
+	@Test
+	public void testGetAccountNumber() throws NoAccountsException, NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
 
-        Customer customer = new Customer();
-        Account account1 = new Account();
-        Account account2 = new Account();
-        
-        Field accountNumbers = Account.class.getDeclaredField("accountNumber");
-        accountNumbers.setAccessible(true);
-        Field initialAmount = Account.class.getDeclaredField("initialAmount");
-        initialAmount.setAccessible(true);
-        
-        Field customerAccounts = Customer.class.getDeclaredField("accounts");
-        customerAccounts.setAccessible(true);
-        
-        List<Account> accounts = new ArrayList<>();
-        
-        accountNumbers.set(account1, "ACCOUNT1");
-        accountNumbers.set(account2, "ACCOUNT2");
-        initialAmount.set(account1, 100.00f);
-        initialAmount.set(account2, 200.00f);
-        
-        accounts.add(account1);
-        accounts.add(account2);
-        customerAccounts.set(customer, accounts);
+		Customer customer = new Customer();
+		Account account1 = new Account();
+		Account account2 = new Account();
 
-        assertEquals(account2.getAccountNumber(), customer.getAccountWithHighestBalance(), "Must return accunt 2.");
- 
-    }
+		Field accountNumbers = Account.class.getDeclaredField("accountNumber");
+		accountNumbers.setAccessible(true);
+		Field initialAmount = Account.class.getDeclaredField("initialAmount");
+		initialAmount.setAccessible(true);
+
+		Field customerAccounts = Customer.class.getDeclaredField("accounts");
+		customerAccounts.setAccessible(true);
+
+		List<Account> accounts = new ArrayList<>();
+
+		accountNumbers.set(account1, "ACCOUNT1");
+		accountNumbers.set(account2, "ACCOUNT2");
+		initialAmount.set(account1, 100.00f);
+		initialAmount.set(account2, 200.00f);
+
+		accounts.add(account1);
+		accounts.add(account2);
+		customerAccounts.set(customer, accounts);
+
+		assertEquals(account2.getAccountNumber(), customer.getAccountWithHighestBalance(), "Must return accunt 2.");
+
+	}
 }
