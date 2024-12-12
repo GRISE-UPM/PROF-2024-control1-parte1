25bf6e3a55ad9e9df77bda01163585502eeddb85
ThemaKinaUPM <123257981+ThemaKinaUPM@users.noreply.github.com>
2024-12-12 18:17:13 +0100

    Uploaded tests question 2 and 3

--- src/test/java/es/upm/grise/prof/curso2024/control1/CustomerTest.java
+++ src/test/java/es/upm/grise/prof/curso2024/control1/CustomerTest.java
@@ -1,8 +1,13 @@
 package es.upm.grise.prof.curso2024.control1;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.Assertions.assertEquals;
-import org.junit.jupiter.api.Assertions.assertThrows;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CustomerTest {
 
@@ -17,7 +22,15 @@ public class CustomerTest {
 	public void testGetAccount(){
 		Customer c = new Customer();
 		Account a = new Account();
-		assertEquals(a,c.getAccountWithHighestBalance())
+		List<Account> accounts = new ArrayList<Account>();
+		accounts.add(a);
+		c.setAccounts(accounts);
+		try {
+			assertEquals(a,c.getAccountWithHighestBalance());
+		} catch (NoAccountsException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 	}
 	
